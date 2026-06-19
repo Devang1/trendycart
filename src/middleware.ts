f8ad7b490ec770d48825780d7b74f253ffb45757
devang1 <devangshukla119@gmail.com>
2026-06-19 15:07:24 +0530

    resolving auth error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,63 @@
-import { withAuth } from "next-auth/middleware";
+// import { withAuth } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
+
+// export default withAuth(
+//   function middleware(req) {
+//     const role = req.nextauth.token?.role;
+//     const pathname = req.nextUrl.pathname;
+
+//     if (pathname.startsWith("/admin") && role !== "ADMIN") {
+//       return NextResponse.redirect(new URL("/", req.url));
+//     }
+
+//     if (
+//       pathname.startsWith("/seller") &&
+//       !["SELLER", "ADMIN"].includes(String(role))
+//     ) {
+//       return NextResponse.redirect(new URL("/", req.url));
+//     }
+
+//     return NextResponse.next();
+//   },
+//   {
+//     secret: process.env.NEXTAUTH_SECRET,
+
+//     callbacks: {
+//       authorized: ({ token, req }) => {
+//         const protectedPaths = [
+//           "/admin",
+//           "/seller",
+//           "/profile",
+//           "/orders",
+//           "/checkout",
+//           "/wishlist",
+//         ];
+
+//         const isProtected = protectedPaths.some((path) =>
+//           req.nextUrl.pathname.startsWith(path)
+//         );
+
+//         return isProtected ? !!token : true;
+//       },
+//     },
+//   }
+// );
+
+// export const config = {
+//   matcher: [
+//     "/admin/:path*",
+//     "/seller/:path*",
+//     "/profile/:path*",
+//     "/orders/:path*",
+//     "/checkout/:path*",
+//     "/wishlist/:path*",
+//   ],
+// };
 import { NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware(req) {
-    const role = req.nextauth.token?.role;
-    const pathname = req.nextUrl.pathname;
-
-    if (pathname.startsWith("/admin") && role !== "ADMIN") {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-
-    if (
-      pathname.startsWith("/seller") &&
-      !["SELLER", "ADMIN"].includes(String(role))
-    ) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-
-    return NextResponse.next();
-  },
-  {
-    secret: process.env.NEXTAUTH_SECRET,
-
-    callbacks: {
-      authorized: ({ token, req }) => {
-        const protectedPaths = [
-          "/admin",
-          "/seller",
-          "/profile",
-          "/orders",
-          "/checkout",
-          "/wishlist",
-        ];
-
-        const isProtected = protectedPaths.some((path) =>
-          req.nextUrl.pathname.startsWith(path)
-        );
-
-        return isProtected ? !!token : true;
-      },
-    },
-  }
-);
+export function middleware() {
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
