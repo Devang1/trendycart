813b2f5f25e8b6f916aba32b2f7f489acb81c84d
devang1 <devangshukla119@gmail.com>
2026-06-19 14:57:55 +0530

    resolving auth error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,22 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
+    console.log("PATH:", req.nextUrl.pathname);
+    console.log("TOKEN:", req.nextauth.token);
+
     const role = req.nextauth.token?.role;
     const pathname = req.nextUrl.pathname;
 
     if (pathname.startsWith("/admin") && role !== "ADMIN") {
+      console.log("Admin access denied");
       return NextResponse.redirect(new URL("/", req.url));
     }
 
-    if (pathname.startsWith("/seller") && !["SELLER", "ADMIN"].includes(String(role))) {
+    if (
+      pathname.startsWith("/seller") &&
+      !["SELLER", "ADMIN"].includes(String(role))
+    ) {
+      console.log("Seller access denied");
       return NextResponse.redirect(new URL("/", req.url));
     }
 
@@ -19,13 +27,35 @@ export default withAuth(
   {
     callbacks: {
       authorized: ({ token, req }) => {
-        const protectedPath = ["/admin", "/seller", "/profile", "/orders", "/checkout", "/wishlist"];
-        return protectedPath.some((path) => req.nextUrl.pathname.startsWith(path)) ? Boolean(token) : true;
-      }
-    }
+        console.log("AUTHORIZED PATH:", req.nextUrl.pathname);
+        console.log("AUTHORIZED TOKEN:", token);
+
+        const protectedPath = [
+          "/admin",
+          "/seller",
+          "/profile",
+          "/orders",
+          "/checkout",
+          "/wishlist",
+        ];
+
+        return protectedPath.some((path) =>
+          req.nextUrl.pathname.startsWith(path)
+        )
+          ? Boolean(token)
+          : true;
+      },
+    },
   }
 );
 
 export const config = {
-  matcher: ["/admin/:path*", "/seller/:path*", "/profile/:path*", "/orders/:path*", "/checkout/:path*", "/wishlist/:path*"]
+  matcher: [
+    "/admin/:path*",
+    "/seller/:path*",
+    "/profile/:path*",
+    "/orders/:path*",
+    "/checkout/:path*",
+    "/wishlist/:path*",
+  ],
 };
